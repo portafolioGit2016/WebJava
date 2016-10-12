3dca806c6db66e1f1578aa53900267e4346ce96b
Roberto <Roberto@WINCTRL-987U00R>
2016-10-11 21:31:54 -0300

    login

---
+++ HawkFlying/src/cl/duoc/hf/LoginVO.java
@@ -0,0 +1,13 @@
+package cl.duoc.hf;
+
+public class LoginVO {
+	private int cantidad;
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+}
--- HawkFlying/src/cl/duoc/hf/service/impl/UserServiceImpl.java
+++ HawkFlying/src/cl/duoc/hf/service/impl/UserServiceImpl.java
@@ -1,12 +1,47 @@
 package cl.duoc.hf.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import cl.duoc.hf.LoginVO;
 import cl.duoc.hf.service.UserService;
 
 public class UserServiceImpl implements UserService{
 
 	@Override
 	public boolean isValidUser(String username, String password) {
-		return true;
+		String uri="https://database-clportafoliotrial.db.us2.oraclecloudapps.com/apex/hawkflying/usuario/login/{user}/{pwd}";
+	     
+	    Map<String, String> params = new HashMap<String, String>();
+	    params.put("user", username);
+	    params.put("pwd", password);
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	    restTemplate.setMessageConverters(getMessageConverters());
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity<String> entity = new HttpEntity<String>(headers);
+	    //LoginVO result = restTemplate.getForObject(uri, LoginVO.class, params);
+	    ResponseEntity<LoginVO> response = restTemplate.exchange(uri, HttpMethod.GET, entity, LoginVO.class, params);
+	    LoginVO resource = response.getBody();	     
+	    System.out.println(resource);
+		return resource.getCantidad()>0;
 	}
-	
+	private List<HttpMessageConverter<?>> getMessageConverters() {
+		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+		converters.add(new MappingJackson2HttpMessageConverter());
+		return converters;
+	}
 }
